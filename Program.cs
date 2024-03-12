fee7b4216f2d75bc7a9972750d8b2ac03878d7cc
Quinn7089 <Quinn7089j@gmail.com>
2024-03-12 17:39:11 -0500

    [1.04] Create ticket class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class ticket
+{
+
+    public string ticketID { get; set; }
+
+    public string Summary { get; set; }
+
+    public string Status { get; set; }
+
+    public string priority { get; set; }
+
+    public string Submitter { get; set; }
+
+    public string assigend { get; set; }
+
+    public string watching { get; set; }
+
+    public string choice { get; set; }
+
+}
